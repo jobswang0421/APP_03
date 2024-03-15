898f2f3d76a39e428963d3c4151063f083531732
JOBS <80052912+jobswang0421@users.noreply.github.com>
2024-03-15 10:38:59 +0800

    WEEK4

--- app/src/main/java/com/example/app_03/MainActivity.kt
+++ app/src/main/java/com/example/app_03/MainActivity.kt
@@ -14,6 +14,11 @@ class MainActivity : AppCompatActivity() {
 
         val applyDate = findViewById<TextView>(R.id.BIRTHDAY)
         val radGrp_Gender = findViewById<RadioGroup>(R.id.radGrpGENDER) // 确保这里使用的ID与布局文件中的ID匹配
+        val tbox1 = findViewById<TextView>(R.id.ACCOUNT)
+        val tbox2 = findViewById<TextView>(R.id.PASSWORD)
+        val tbox3 = findViewById<TextView>(R.id.NAME)
+        val tbox4 = findViewById<TextView>(R.id.EMAIL)
+        val tbox5 = findViewById<TextView>(R.id.PHONE)
         val chkbox1 = findViewById<CheckBox>(R.id.INTEREST1)
         val chkbox2 = findViewById<CheckBox>(R.id.INTEREST2)
         val chkbox3 = findViewById<CheckBox>(R.id.INTEREST3)
@@ -32,28 +37,29 @@ class MainActivity : AppCompatActivity() {
         }
 
         btn_send.setOnClickListener {
-            
-            var msg = "興趣:\n"
-            if (chkbox1.isChecked) msg += "${chkbox1.text}\n"
-            if (chkbox2.isChecked) msg += "${chkbox2.text}\n"
-            if (chkbox3.isChecked) msg += "${chkbox3.text}\n"
+
+            var msg = getString(R.string.account)+": ${tbox1.text}\n"
+            msg += getString(R.string.password)+": ${tbox2.text}\n"
+            msg += getString(R.string.name)+": ${tbox3.text}\n"
+            msg += getString(R.string.email)+": ${tbox4.text}\n"
+            msg += getString(R.string.phone)+": ${tbox5.text}\n"
+            msg += getString(R.string.bd)+": ${applyDate.text}\n"
 
             val selectedGenderId = radGrp_Gender.checkedRadioButtonId
             val gender = when (selectedGenderId) {
-                R.id.MALE -> "性别: 男"
-                R.id.FEMALE -> "性别: 女"
-                else -> "性别: 未知"
+                R.id.MALE -> msg += getString(R.string.gender)+": "+getString(R.string.male)+"\n"
+                R.id.FEMALE -> msg += getString(R.string.gender)+": "+getString(R.string.female)+"\n"
+                else -> msg += getString(R.string.gender)+": "+getString(R.string.idk)+"\n"
             }
-            msg += "\n$gender\n"
+            msg += getString(R.string.interest)+":\n"
+            if (chkbox1.isChecked) msg += "          ${chkbox1.text}\n"
+            if (chkbox2.isChecked) msg += "          ${chkbox2.text}\n"
+            if (chkbox3.isChecked) msg += "          ${chkbox3.text}\n"
 
-            val birthday = applyDate.text.toString()
-            msg += "生日: $birthday"
-
-            // 显示对话框
             AlertDialog.Builder(this).apply {
-                setTitle("确认信息")
+                setTitle(R.string.check)
                 setMessage(msg)
-                setPositiveButton("确定") { dialog, _ ->
+                setPositiveButton(R.string.ok) { dialog, _ ->
                     dialog.dismiss()
                 }
                 show()
